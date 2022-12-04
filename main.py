a4a5c565eea8d77c55362c8e76e2ae47ea499c0a
MatthieuDlhy <matthieudlhy@gmail.com>
2022-12-04 21:31:27 +0100

    third commit

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250999446-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' UA-250999446-1');
+</script>
+ """
+ return prefix_google + "Hello World"
