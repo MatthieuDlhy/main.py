6c79734bb26910ad42c7aba990fd7c84b3878a8b
MatthieuDlhy <matthieudlhy@gmail.com>
2022-12-20 16:56:03 +0100

    test python logger

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -16,3 +16,34 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250999446-1"></script>
 </script>
  """
  return prefix_google + "Hello World"
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/logger/')
+# def logger():
+#     return render_template('about.html')
+
+# import logging
+
+# @app.route('/logger', methods=["GET"])
+# def printMsg():
+#     app.logger.warning('testing warning log')
+#     app.logger.error('testing error log')
+#     app.logger.info('testing info log')
+#     return "Check your console"
+
+@app.route('/logger', methods=['GET'])
+def printMsg():
+    loginfo = "This is my log"
+    app.logger.warning(loginfo)
+    app.logger.error(loginfo)
+    app.logger.info(loginfo)
+
+    return render_template('log.html', loginfo = "This is my log")
+
+# import sys
+
+# print('This is error output', file=sys.stderr)
+# print('This is standard output', file=sys.stdout)
